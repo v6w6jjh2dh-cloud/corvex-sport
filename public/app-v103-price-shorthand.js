e9cddf620776b193d70776e85a014a6486a05557
v6w6jjh2dh-cloud <v6w6jjh2dh@privaterelay.appleid.com>
2026-08-30 10:21:35 +0300

    Safely support dinar shorthand in order prices

---
+++ public/app-v103-price-shorthand.js
@@ -0,0 +1,7 @@
+(()=>{
+ const expand=value=>String(value||'').replace(/(^|\s)(-?[٠-٩۰-۹\d]+(?:[.,][٠-٩۰-۹\d]+)?)\s*د(?=\s*(?:شامل|مع)\s+التوصيل(?:\s|$))/giu,'$1$2 دينار');
+ const originalPrice=globalThis.priceFrom;
+ if(typeof originalPrice==='function')globalThis.priceFrom=line=>originalPrice(expand(line));
+ const originalInstruction=globalThis.instructionFromPriceLine;
+ if(typeof originalInstruction==='function')globalThis.instructionFromPriceLine=line=>originalInstruction(expand(line));
+})();
